d172d36d4b819af151569b3ed1136ae8eae7cb20
obadah <obadahmthmth@gmail.com>
2021-09-23 11:06:21 +0200

    Functions

--- src/index.js
+++ src/index.js
@@ -14,3 +14,21 @@ function getWords() {
 
     return json;
 }
+
+/**
+ * @function createRandomNumber
+ * @description Creates a random number according to a minimal and maximal value
+ * @param{number} minNumber
+ * @param{number} maxNumber
+ * @returns{number}
+ */
+
+
+function createRandomNumber(minNumber, maxNumber) {
+
+    const num = Math.floor(Math.random() * (maxNumber - minNumber) + minNumber);
+
+    // random =0.5* 50000-10000=15000+25000
+    // wir geben die zufällige nummer zrück
+    return num;
+}
